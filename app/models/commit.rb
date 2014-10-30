ff88ff9c1ff0327d850a43f84bc30c9965c9d50f
pavelperkin <perkinpv@gmail.com>
2014-10-31 00:02:37 +0200

    Cleanup.

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -4,13 +4,9 @@ class Commit < ActiveRecord::Base
   validates :url, presence: true,
                   uniqueness: true,
                   format: { with: URI.regexp }
-
   validates :message, presence: true
-
   validates :sha, presence: true
-
   validates :commited_at, presence: true
-
   validates :user_id, presence: true,
                       numericality: { only_integer: true }
 end
