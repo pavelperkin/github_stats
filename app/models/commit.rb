ce06250f87ca636e4ed1475b585dd243317045cb
pavelperkin <perkinpv@gmail.com>
2014-10-31 00:01:54 +0200

    Create Commit model. User has many commits.

---
+++ app/models/commit.rb
@@ -0,0 +1,16 @@
+class Commit < ActiveRecord::Base
+  belongs_to :user
+  
+  validates :url, presence: true,
+                  uniqueness: true,
+                  format: { with: URI.regexp }
+
+  validates :message, presence: true
+
+  validates :sha, presence: true
+
+  validates :commited_at, presence: true
+
+  validates :user_id, presence: true,
+                      numericality: { only_integer: true }
+end
