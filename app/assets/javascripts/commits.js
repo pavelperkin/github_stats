2cd8697bc708ff2a63b1906767b5263a4c21e3e6
pavelperkin <perkinpv@gmail.com>
2014-11-03 20:14:37 +0200

    Use JST for new commit.

--- app/assets/javascripts/commits.js
+++ app/assets/javascripts/commits.js
@@ -2,17 +2,12 @@ $(document).ready(function(){
   var source = new EventSource('/commits/stream');
   source.addEventListener('results', function(e){
     if(e.data != "null"){
-      commit = jQuery.parseJSON(e.data)
-      // #TODO Set templates for commits and render them here.
-      $("#commits tbody").prepend(
-        "<tr><td>"+commit.id+"</td><td>"+commit.username+"</td><td>"+
-        commit.message+"</td><td>"+commit.sha+"</td><td>"+commit.url+"</td></tr>"
-      );
-    } 
+      commit = jQuery.parseJSON(e.data);
+      html = JST["templates/commit"](commit);
+      $("#commits tbody").prepend(html);
+    }
   });
   source.addEventListener('finished', function(e){
     source.close();
   });
 });
-
-
