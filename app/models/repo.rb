2eeac3a67d5287edf189abc8f783ad28f8da8e1b
pavelperkin <perkinpv@gmail.com>
2014-11-06 14:53:16 +0200

    MIR. Get user data from commit.

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -2,20 +2,32 @@ class Repo < ActiveRecord::Base
   has_many :commits
   belongs_to :organization
 
-  scope :active, -> { where(observed: true) }
-  scope :inactive, -> { where(observed: false) }
+  after_update :get_todays_commits, if: Proc.new { |obj| obj.observed }
 
-  # after_create :get_today_commits
-
-  def get_today_commits
-    Octokit.commits_since("#{self.organization.name}/#{self.name}", Date.today).each do |commit|
-      self.commits.create(user_id: 1,
+  def get_todays_commits
+    Octokit.commits_since("#{organization.name}/#{name}", Date.today).each do |commit|
+      commits.create(user_id: get_user_id(commit.author),
                           message: commit.commit.message,
                           url: commit.html_url,
                           sha: commit.sha,
                           commited_at: commit.commit.committer.date,
-                          organization_id: self.organization)
+                          organization_id: organization.id)
     end
+  end
+
+  def get_user_id(data)
+    (find_user(data) || create_user(data)).id
+  end
+
+  def find_user(data)
+    User.find_by(github_id: data.id)
+  end
+
+  def create_user(data)
+    User.create(github_id: data.id,
+                login: data.login,
+                url: data.html_url,
+                avatar_url: data.avatar_url)
   end
 end
 
