3c11518c0e97d6b1197eb9242ab2bcdc624f04de
pavelperkin <perkinpv@gmail.com>
2014-11-11 05:26:57 +0200

    users specs.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe User, type: :model do
+
+  describe '#find_user' do
+    context 'if user exist' do
+      it 'should return this user' do
+        user = create(:user)
+        mock = OpenStruct.new(id: 123456)
+        expect(User.find_user(mock).id).to eql(user.id)
+      end
+    end
+    context 'if user does not exist' do
+      it 'should return nil' do
+        mock = OpenStruct.new(id: 654321)
+        expect(User.find_user(mock)).to eql(nil)
+      end
+    end
+  end
+
 end
