707317e3295b36051c320850f590a8ba0ad12754
pavelperkin <perkinpv@gmail.com>
2014-11-11 03:31:48 +0200

    Add specs for organization model.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
-RSpec.describe Organization, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Organization, type: :model do
+
+  describe 'before_validation#strip_name' do
+    it 'should strip name before save' do
+      unstripped_name = '   rails    '
+      org = create(:organization, name: unstripped_name)
+      expect(org.name).to eql(unstripped_name.strip)
+    end
+  end
+
+  describe 'validation#should_exist_on_github' do
+    it 'should rollback if organization does not presence on github' do
+      org = build(:organization, name: 'some_crazy_string'.reverse)
+      expect(org.valid?).to eql false
+    end
+  end
+
+  describe 'callback set_url' do
+    it 'should get org url from github' do
+      org = create(:organization)
+      expect(org.url).to eql('https://github.com/rails')
+    end
+  end
+
+  describe 'callback get org repos' do
+    it 'should fetch org repos after create' do
+      org = create(:organization)
+      expect(org.repos.any?).to eql true
+    end
+  end
+
 end
